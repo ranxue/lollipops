79344e7600f60243786366c62cd65f5b5ebb2c8f
ranxue <xueran2010@gmail.com>
2015-09-30 18:34:56 -0400

    add original lollipops

--- drawing/draw.go
+++ drawing/draw.go
@@ -100,8 +100,8 @@ func (s *Settings) prepare(changelist []string, g *data.PfamGraphicResponse) *di
 		}
 		s.GraphicHeight += maxStaggered
 		startY += maxStaggered - (s.LollipopRadius + s.LollipopHeight)
-		fmt.Fprintf(os.Stderr,  s.LollipopRadius)
-		fmt.Fprintf(os.Stderr,  s.LollipopHeight)
+		fmt.Fprintf("%e\n",  s.LollipopRadius)
+		fmt.Fprintf("%e\n",  s.LollipopHeight)
 	}
 	if !s.HideAxis {
 		s.GraphicHeight += s.AxisPadding + s.AxisHeight
