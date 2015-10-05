c2fb09b037192345b477c7506d28bf90680780e6
ranxue <xueran2010@gmail.com>
2015-10-05 12:27:20 -0400

    update resizing feature

--- drawing/svg.go
+++ drawing/svg.go
@@ -8,7 +8,7 @@ import (
 )
 
 const svgHeader = `<?xml version='1.0'?>
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="%f" height="%f">
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox="0 0 800 600" >
 <defs>
   <filter id="ds" x="0" y="0">
     <feOffset in="SourceAlpha" dx="2" dy="2" />
